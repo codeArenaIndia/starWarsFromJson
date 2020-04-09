fefefea0f2604d839eea7b94a3c707ee057efb9b
codeArenaIndia <umairkhan198@gmail.com>
2020-04-09 19:47:34 +0530

    navigation corrected

--- src/pages/Planets.js
+++ src/pages/Planets.js
@@ -1,17 +1,15 @@
 import React , {useState } from 'react';
-import { handleNavigationHelper, debounce ,updateCounter } from '../Components/Helper/Helper'
+import { debounce ,updateCounter } from '../Components/Helper/Helper'
 import PlanetHeader from '../Components/Presentational/Planet-Header';
 import PlanetBody from '../Components/Presentational/Planet-Body';
 import { SearchInput } from '../Components/Input'
-//import axios from 'axios';
 import "../Components/Helper/Style/Planets.css";
 import PlanetList from '../Data/Planets.json';
+import { useHistory } from 'react-router-dom'
+
 
 export default function Planets(){
   const [planets,setPlanets] = useState([]);
-// const [page,setPage] = useState(1)
-//const [count,setCount] = useState(null);
- // const [next,setNext] = useState(null);
   const [query,setQuery] = useState("");
   const [loading,setLoading] = useState("none");
   const [restricted,setRestricted] = useState("");
@@ -20,6 +18,7 @@ export default function Planets(){
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  let history = useHistory();
 
   /****************************Mounting,Unmounting and Updating code starts*****************/
   
@@ -37,7 +36,6 @@ export default function Planets(){
      const res = Object.entries(PlanetList);
      const newPlanetList = [];
       res.forEach((item,index)=>{
-        //newPlanetList[index] = Object.entries(item[1].fields);
         if((item[1].fields.name.toLowerCase()).includes(queries.toLowerCase())){
           newPlanetList[index] = item[1].fields;
         }
@@ -46,23 +44,6 @@ export default function Planets(){
         console.log("item",item)
       });
      setPlanets(newPlanetList);
-    // setNext("done");
-   //  setCount(newPlanetList.length);
-      
-
-
-  //TO get data FROM API UNCOMMENT THE BELOW  LINES 
-    /*  const res =  await axios.get(`https://swapi.co/api/planets/?page=${pages}&format=json&search=${queries}`);
-      let response= res.data; 
-      let maxPage = response.count/10;
-        if(planets.length !== 0){
-          let res = [...planets,...response.results];
-          setPlanets(res);
-        }else {
-          setPlanets(response.results);
-        }
-        setCount(response.count);
-        maxPage > page ?  setNext(response.next) : setNext("done"); */
 
     } catch (err){
         console.log('Error',err);
@@ -73,9 +54,14 @@ export default function Planets(){
   const handleSearch = debounce(function(target) {
     setPlanets([]);
     setQuery(target.value);
-   // setPage(1);
   }, 600);
   
+  const local = localStorage.getItem("user");
+  React.useEffect(()=>{
+    if( local === null){
+      history.push("/login");
+    }
+  },[local]);
 
   React.useEffect(()=>{
     getResults(query);
@@ -99,8 +85,6 @@ export default function Planets(){
     <div className="planetContainer col-md-12 npr">
       <PlanetHeader username={username}/>
       <SearchInput handleSearch={handleSearch}/>
-     {/* <button type="button" className="loadmore-btn btn btn-success transparent pull-left" data-pointer={loading} onClick={event => setPage(handleNavigationHelper(event.target))} data-nav={next}>{loading === "block" ? "loading..." : "Load More"}</button>
-    <p  className="count col-md-2 pull-left text-white">Showing: {count}</p> */}
       <PlanetBody handleShowModal={handleShowModal} restricted={restricted} planets={planets} handleClose={handleClose} modalData={modalData} show={show} loading={loading}/>
     </div>
   )
